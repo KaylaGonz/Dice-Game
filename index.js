bbe555200f7ad82592d741bc3f99c6f35c6c0534
KaylaGonz <80657481+KaylaGonz@users.noreply.github.com>
2022-02-24 22:32:03 -0800

    Add initial files

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var image1 = document.querySelectorAll("img")[0]; //All image tags in an array
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!"; //use querySelector if there is only one of element
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
